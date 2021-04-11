1fb8ec1fcb20603ce432ba82dcb94664d139f9ad
Zbigniew Gawroński <zbigniew.gawronski@microsoft.wsei.edu.pl>
2021-04-11 11:10:13 +0200

    lab3

--- lab3/src/app.ts
+++ lab3/src/app.ts
@@ -1,15 +1,30 @@
 export class App {
     opwApiKey = '848eefdeeb4818cfa7b6b9fb9a281dea';
     constructor() {
-        this.getData();
+        this.getCityInfo('zakopane');
     }
-    async getData() {
-        const weather = await this.getWeather('chrzanów');
-        this.saveData();
+    async getCityInfo(city: string) {
+        const weather = await this.getWeather('zakopane');
+        this.saveData(weather);
     }
 
     async getWeather(city: string): Promise<any> {
-        const openWeatherUrl = 'http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.opwAPIKey}'
+        const openWeatherUrl = 'http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.opwAPIKey}';
+        const weatherResponse = await fetch(openWeatherUrl);
+        const weatherData = await weatherResponse.json();
+        console.log(weatherData);
+        return weatherData;
+    }
+    saveData(data: any) {
+        localStorage.setItem('weatherData', JSON.stringify(data));
+
+    }
+    getData() {
+        const data = localStorage.getItem('weatherData');
+        if (data) {
+            return JSON.parse(data);
+        } else {
+            return {};
+        }
     }
 }
-export const AAA = 10;
