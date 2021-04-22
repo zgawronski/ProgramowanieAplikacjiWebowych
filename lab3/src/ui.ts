5d066e3039133f510b6345723289ab0089bf2435
Zbigniew Gawroński <zbigniew.gawronski@microsoft.wsei.edu.pl>
2021-04-22 13:07:25 +0200

    lack of refresh

--- lab3/src/ui.ts
+++ lab3/src/ui.ts
@@ -1,34 +1,28 @@
 import { App } from './app';
 const app = new App();
-
 export class Ui {
-
-    private customWindow: HTMLElement;
-    private cityName: HTMLElement;
-    private imgEl: HTMLImageElement;
-    private description: HTMLElement;
-
-
 
     constructor(){
         this.customWind('div');
     };
 
-    customWind(element: any){
+    async customWind(element: any){
         const customWind = document.createElement('div');
         customWind.className = "cWind";
         customWind.setAttribute("id", "customWindId");
         document.body.appendChild(customWind);
         const dane1 = document.createElement('div');
         dane1.setAttribute("id", "description");
-        const dane2 = document.createElement('h1');
+        const dane2 = document.createElement('h3');
         dane2.setAttribute("id", "temp");
         const dane3 = document.createElement('div');
         dane3.setAttribute("id", "location");
-        const weatherDane = app.getCityInfo();
+        const weatherDane = await app.getCityInfo();
 
-        //dane1.innerHTML = weatherDane.name;
-        console.log(weatherDane);
+
+        dane1.innerHTML = weatherDane.name;
+        dane2.innerHTML = "temperatura: " + Math.round(weatherDane.main.temp - 273.15).toString() + '&deg;';
+        dane3.innerHTML = "wilgotność: " + weatherDane.main.humidity + "%";
         customWind.appendChild(dane1);
         customWind.appendChild(dane2);
         customWind.appendChild(dane3);
