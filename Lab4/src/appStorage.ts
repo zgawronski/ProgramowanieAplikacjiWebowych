1a39a2abf75fca413e93940cc1b93e91462facb4
Zbigniew Gawroński <zbigniewgawro@gmail.com>
2021-06-04 10:34:22 +0200

    lf solution

--- Lab4/src/appStorage.ts
+++ Lab4/src/appStorage.ts
@@ -10,53 +10,54 @@ export class AppStorage {
     }
 
     constructor() {
-        this.addNote();
+        this.addNote('note');
     };
 
 
-    async addNote() {
+    async addNote(note: any) {
         const buttonAdd = document.getElementById('AddButton');
         buttonAdd.addEventListener('click', async (ev: Event) => {
             const takeTitle = <HTMLInputElement>document.getElementById('AddNote');
             const takeContent = <HTMLInputElement>document.getElementById('content');
             const contentNote = takeContent.value;
             const title = takeTitle.value;
-            if ((title === '') || (this.getNote(title)) || (title === 'null')) { }
+            if ((title === '') || (title === 'null')) { }
             else {
                 takeTitle.value = '';
                 takeContent.value = '';
             }
 
             return {
-                title,
-                contentNote
+                note
             }
         });
 
         //interface notatki
         const notatka: IAppStorage = {
             id: 1,
-            title: buttonAdd.title,
-            content: buttonAdd.contentNote,
+            title: note.takeTitle.value,
+            content: note.takeContent.value,
             dateOfNote: new Date().toDateString(),
         }
         this.saveData(notatka)
 
+        console.log(notatka)
     }
 
-    private getNote(newNote: string) {
-        let nameExist = false;
-        const data = localStorage.getItem('note');
-        if (data != null) {
-            const noteColection = JSON.parse(data) as any[];
-            noteColection.forEach((x) => {
-                if (x.toLowerCase() == newNote.toLowerCase())
-                    nameExist = true;
-            });
-            return nameExist;
-        }
-    }
+    // private getNote(newNote: string) {
+    //     let nameExist = false;
+    //     const data = localStorage.getItem('note');
+    //     if (data != null) {
+    //         const noteColection = JSON.parse(data) as any[];
+    //         noteColection.forEach((x) => {
+    //             if (x.toLowerCase() == newNote.toLowerCase())
+    //                 nameExist = true;
+    //         });
+    //         return nameExist;
+    //     }
+    // }
 
 }
+
 
 
